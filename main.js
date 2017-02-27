bf979b7d1040f46d51c970ec5d9e4e750a9faa7e
Alessandro Lettieri <nano@welance.de>
2017-02-27 17:38:11 +0100

    First Commit with basic webpack configuration

---
+++ main.js
@@ -0,0 +1,2 @@
+const css = require("./sass/main.scss"); //webpack-dev
+console.log("hello World"); 
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path')
+
+module.exports = {
+    entry: "./main.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallbackLoader: "style-loader",
+                    loader: ["css-loader", "sass-loader"],
+                    publicPath: "/dist"
+                })
+            }
+        ]
+    },
+    plugins: [
+                new HtmlWebpackPlugin({
+                        title: "Welance",
+                        filename: "index.html",
+                        template: "./index.html",
+                        minify:{
+                            collapseWhitespace: true
+                    }}),
+                new ExtractTextPlugin({
+                        filename: "bundle.css"
+                    })
+            ]
+};
