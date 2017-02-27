689bd291c8a7c365eb4b4bc6c85150c49584c504
Enrico Icardi <enrico.icardi@gmail.com>
2017-02-27 18:09:03 +0100

    missing semicolon

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var path = require('path')
+var path = require('path');
 
 module.exports = {
     entry: "./main.js",
