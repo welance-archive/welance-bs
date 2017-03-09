07fb899cf65bb23ba96cbd4ecd394859c813f0b1
Enrico Icardi <enrico.icardi@gmail.com>
2017-03-09 20:00:33 +0100

    ~ refactored, simplified and made gallery reusable + added general styles for background images display in welanstrap

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,34 +1,26 @@
 <template>
   <div class="section">
-      <div class="gallery">
-        <div class="gallery__container gallery__container--off-canvas">
-            <div class="gallery__col gallery__col--odd-position">
-                <img class="img img-square" src="https://placehold.it/400x300"/>
-            </div>
-            <div class="gallery__col gallery__col--odd-position">
-                <img class="img img-square" src="https://placehold.it/300x400"/>
-            </div>
-            <div class="gallery__col gallery__col--odd-position">
-                <img class="img img-square" src="https://placehold.it/400x300"/>
-            </div>
-            <div class="gallery__col gallery__col--odd-position">
-                <img class="img img-square" src="https://placehold.it/300x400"/>
-            </div>
-        </div>
+    <div class="gallery">
+      <div class="gallery__container gallery__container--off-canvas">
+        <div class="img img--ratio-9-16" style="background-image:url('http://fillmurray.com/400/400');"> </div>
+        <div class="img img--ratio-12-16" style="background-image:url('http://fillmurray.com/400/400');"> </div>
+        <div class="img img--ratio-9-16" style="background-image:url('http://fillmurray.com/400/400');"> </div>
+        <div class="img img--ratio-12-16" style="background-image:url('http://fillmurray.com/400/400');"> </div>
+        <div class="img img--ratio-9-16" style="background-image:url('http://fillmurray.com/400/400');"> </div>
+        <div class="img img--ratio-12-16" style="background-image:url('http://fillmurray.com/400/400');"> </div>
+        <div class="img img--ratio-9-16" style="background-image:url('http://fillmurray.com/400/400');"> </div>
+        <div class="img img--ratio-12-16" style="background-image:url('http://fillmurray.com/400/400');"> </div>
+      </div>
     </div>
-
   </div>
 </template>
 
 <script>
 export default {
-    name: 'Gallery',
-    data () {
-        return{
-            text: 'Image Row',
-			smallText: 'About Us'
-        }
-    }
+  name: 'Gallery',
+  data () {
+    return{}
+  }
 };
 </script>
 
@@ -39,65 +31,67 @@ export default {
 .section{
   //background: red;
   overflow: hidden;
-  @include mq($until: 'md'){
-    padding: 0 3em;
-    overflow: hidden;
-  }
-
-
 }
 
 .gallery {
-//
-  //margin: 0 auto;
-    @include mq($from: 'xl'){
-        @include make-container-max-widths();
-        margin:  0 auto;
-    }
-  padding: 10vh 0px;
-  position: relative;
+  @include make-container-max-widths();
+  margin: 0 auto;
 
   &__container{
-    @include make-row();
-    flex-direction: row;
-    padding: 0px;
-    margin: 0 auto;
-    align-items: flex-end;
+    flex-flow: row wrap;
+    justify-content: space-around;
 
+    // Mode: blocky
+    //
+    // Feature List:
+    // T.B.D with @nano
+    //
+    &--blocky{
+      @include make-row();
+      padding: .5vw;
+
+      .img{
+        flex: auto;
+        margin: .5vw;
+      }
+    }
+
+    // Mode: off-canvas
+    //
+    // Feature List:
+    // 1) respect image sizes ratios
+    // 2) flex-end alignement for big displays
+    // 3) vertically centered alignement for medium displays
+    // 4) 100% responsive on small displays
     &--off-canvas{
-        overflow: hidden;
-        position: relative;
-        width: 120%;
-        left: -10%;
-        right: -10%;
+      @include make-row();
+      flex-flow: row nowrap;
 
+      .img{
+        align-self: center;
+        border: 16px solid white;
+        &:last-child{
+          border-right: none;
+        }
+        &:first-child{
+          border-left: none;
+        }
+
+        @include mq($from: 'xl'){
+          align-self: flex-end;
+        }
+      }
+
+      @include mq($until: 'md'){
+        flex-flow: row wrap;
+        .img{
+          border: none;
+          border-top: 16px solid white;
+          width: 100%;
+        }
+      }
     }
  }
-
-  &__col{
-    @include make-col-ready();
-    @include make-col(3);
-
-        @include mq($until: 'md'){
-            @include make-col(12);
-            margin-bottom:30px;
-        }
-    &--odd-position{
-        &:nth-child(1){
-            align-self: flex-start;
-
-            @include mq($from: 'xl'){
-                align-self: flex-end;
-            }
-        }
-    }
-
-	}
-
-  img{
-    width: 100%;
-  }
-
 };
 
 </style>
