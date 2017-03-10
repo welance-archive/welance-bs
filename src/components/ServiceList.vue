7ff40d7bb851d4514978d7dde05df439ad02b6aa
Enrico Icardi <enrico.icardi@gmail.com>
2017-03-10 19:37:27 +0100

    - removing “scoped” keyword from all components

--- src/components/ServiceList.vue
+++ src/components/ServiceList.vue
@@ -94,7 +94,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scoped>
+<style lang="scss">
 @import "~styles/main.scss";
 
 .service-list {
--- src/components/common/Gallery.vue
+++ src/components/common/Gallery.vue
@@ -52,7 +52,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scoped>
+<style lang="scss">
 @import "~styles/main.scss";
 
 .gallery-wrapper{
